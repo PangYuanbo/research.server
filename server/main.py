a4c7583e141b3bdea941079f48640124bd89095c
PangYuanbo <147596230+PangYuanbo@users.noreply.github.com>
2024-04-27 16:41:30 -0700

    db cdelate

--- server/main.py
+++ server/main.py
@@ -4,7 +4,7 @@ from fastapi import Depends
 from sqlalchemy.orm import Session
 from propelauth_fastapi import init_auth
 import os
-
+from server.routers import datachange
 from propelauth_py.user import User
 
 from server.db import schemas
@@ -16,7 +16,4 @@ auth = init_auth(PROPELAUTH_AUTH_URL, PROPELAUTH_API_KEY)
 
 
 app= FastAPI()
-
-@app.get("/api/whoami")
-async def root(current_user: User = Depends(auth.require_user)):
-    return {"user_id": f"{current_user.user_id}"}
+app.include_router(datachange.router)
