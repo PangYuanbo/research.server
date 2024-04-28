047e581ceb733cd7762df1ff9d33907d6e565c7a
PangYuanbo <147596230+PangYuanbo@users.noreply.github.com>
2024-04-27 17:06:47 -0700

    db finall

--- server/db/schemas.py
+++ server/db/schemas.py
@@ -1,19 +1,40 @@
-# schemas.py
-from pydantic import BaseModel, UUID4, Field
+from pydantic import BaseModel, UUID4, Field, EmailStr
 from typing import Optional
 
-class UserUpdate(BaseModel):
+
+# Shared properties
+class UserBase(BaseModel):
     name: Optional[str] = None
     professor: Optional[bool] = None
+    lastname: Optional[str] = None
+    firstname: Optional[str] = None
+    gender: Optional[str] = None
+    pronounce: Optional[str] = None
+    biography: Optional[str] = None
+    eduemail: Optional[EmailStr] = None
+    phonenumber: Optional[int] = None
+    personal_homepage: Optional[str] = None
+    featured_publications: Optional[str] = None
+    award_honor: Optional[str] = None
+    department: Optional[str] = None
+    photo: Optional[str] = None
+    research_area: Optional[int] = None
 
-class UserCreate(BaseModel):
+
+# Properties to receive via API on creation
+class UserCreate(UserBase):
     name: str
     professor: bool
 
 
-class User:
+# Properties to receive via API on update
+class UserUpdate(UserBase):
+    pass
+
+
+# Properties to return to client
+class User(UserBase):
     id: UUID4
-    name: str
-    professor: bool
+
     class Config:
         orm_mode = True
--- server/main.py
+++ server/main.py
@@ -1,14 +1,9 @@
-from typing import Union
+
 from fastapi import FastAPI
-from fastapi import Depends
-from sqlalchemy.orm import Session
 from propelauth_fastapi import init_auth
 import os
 from server.routers import data
-from propelauth_py.user import User
 
-from server.db import schemas
-from server.db.dependencies import get_db
 
 PROPELAUTH_API_KEY = os.environ.get("PROPELAUTH_API_KEY")
 PROPELAUTH_AUTH_URL="https://58121323173.propelauthtest.com"
